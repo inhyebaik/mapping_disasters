4206c9f17e2949f1d41aa4bb131b3129fc1c8348
Inhye <inb125@mail.harvard.edu>
2018-02-10 10:50:14 -0800

    start setting up model

--- model.py
+++ model.py
@@ -3,3 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 """ Data model for disasters. """
 db = SQLAlchemy()
 
+class Disaster(db.Model):
+    """ Disaster object. """
+
+    __tablename__ = 'disasters'
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    disaster_num = db.Column(db.Integer, nullable=True)
+    state = db.Column(db.String(10), nullable=True)
+    disaster_type = db.Column(db.String(10), nullable=True)
+    incident_type = db.Column(db.String(100), nullable=True)
+    title = db.Column(db.String(200), nullable=True)
+
+    
