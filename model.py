677cc8113459c4baaa106bce291f5bcc60c6ed72
Inhye <inb125@mail.harvard.edu>
2018-02-10 10:53:12 -0800

    finish basic model

--- model.py
+++ model.py
@@ -1,7 +1,10 @@
+""" Data model for disasters. """
+
 from flask_sqlalchemy import SQLAlchemy
 
-""" Data model for disasters. """
 db = SQLAlchemy()
+
+#---------------------------------------------------------------------#
 
 class Disaster(db.Model):
     """ Disaster object. """
@@ -14,5 +17,31 @@ class Disaster(db.Model):
     disaster_type = db.Column(db.String(10), nullable=True)
     incident_type = db.Column(db.String(100), nullable=True)
     title = db.Column(db.String(200), nullable=True)
+    start = db.Column(db.DateTime, nullable=True)
+    end = db.Column(db.DateTime, nullable=True)
+    closeout = db.Column(db.DateTime, nullable=True)
 
-    
+    def __repr__(self):
+        """ Cleaner representation of disaster. """
+
+        repr_str = "<Disaster id={id} state={state}>"
+        return repr_str.format(id=self.id, state=self.state)
+
+#---------------------------------------------------------------------#
+
+def connect_to_db(app):
+    """Connect the database to Flask app."""
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///disasters'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+if __name__ == "__main__":
+    from server import app
+    connect_to_db(app)
+    print "Connected to DB."
+
+
+
+
